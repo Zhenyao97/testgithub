7179765087f64140a721f8f7d63ff61b9a79c45a
Zhenyao97 <1578325528@qq.com>
2020-01-03 14:53:49 +0800

    modified test.c

---
+++ test.c
@@ -0,0 +1,21 @@
+/*================================================================
+*   Copyright (C) 2020 Zhenyao Ltd. All rights reserved.
+*   
+*   文件名称：test.c
+*   创 建 者：Luo
+*   创建日期：2020年01月03日
+*   描    述：
+*
+================================================================*/
+
+
+#include <stdio.h>
+
+
+int main(int argc, char *argv[])
+{
+	print("hello world\n");	
+	
+	return 0;
+}
+
